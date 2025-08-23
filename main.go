d2a62eccfbf6eeb4813c9cf6f93054b445b5d31a
b9nk <wackoalt@gmail.com>
2025-08-23 22:15:44 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
-// main.go
-
 package main
 
 import (
 	"fmt"
-	"krunker-cli/services"
+	"krunker-checker/services"
+)
+
+const (
+	ColorCyan  = "\033[36m"
+	ColorReset = "\033[0m"
 )
 
 func main() {
-	fmt.Println("Starting account checking...")
-
 	if err := services.ProcessAccounts(); err != nil {
 		fmt.Printf("Error processing accounts: %v\n", err)
 		return
 	}
 
-	fmt.Println("\nchecking completed!")
-	fmt.Println("Check the results.")
+	fmt.Println(ColorCyan + "\nchecking completed!" + ColorReset)
+	fmt.Println(ColorCyan + "Check the results." + ColorReset)
 }
