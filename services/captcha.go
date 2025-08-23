748b935fb4cb3d30d55651a8f765bbc267da1924
b9nk <wackoalt@gmail.com>
2025-08-23 22:14:02 +0400

    Update captcha.go

--- services/captcha.go
+++ services/captcha.go
@@ -1,3 +1,5 @@
+// services/captcha.go
+
 package services
 
 import (
@@ -16,28 +18,22 @@ func SolveCaptcha(conn *websocket.Conn, captchaMessage []interface{}) error {
 	if len(captchaMessage) < 2 {
 		return fmt.Errorf("invalid captcha message")
 	}
-
 	captchaData, ok := captchaMessage[1].(map[string]interface{})
 	if !ok {
 		return fmt.Errorf("invalid captcha data format")
 	}
-
 	algorithm := toString(captchaData, "algorithm")
 	challenge := toString(captchaData, "challenge")
 	salt := toString(captchaData, "salt")
 	signature := toString(captchaData, "signature")
 	maxNumber := toIntFromAny(captchaData["maxnumber"])
-
 	fmt.Printf("Solving captcha: algorithm=%s, challenge=%s, maxnumber=%d\n", algorithm, challenge, maxNumber)
-
 	if maxNumber <= 0 {
 		return fmt.Errorf("invalid maxnumber: %d", maxNumber)
 	}
-
 	startTime := time.Now()
 	solution := solveChallengeSync(algorithm, challenge, salt, maxNumber)
 	elapsed := int(time.Since(startTime).Milliseconds())
-
 	solutionData := map[string]interface{}{
 		"algorithm": algorithm,
 		"challenge": challenge,
@@ -46,25 +42,20 @@ func SolveCaptcha(conn *websocket.Conn, captchaMessage []interface{}) error {
 		"signature": signature,
 		"took":      elapsed,
 	}
-
 	solutionJSON, err := json.Marshal(solutionData)
 	if err != nil {
 		return fmt.Errorf("failed to marshal solution: %w", err)
 	}
-
 	solutionB64 := base64.StdEncoding.EncodeToString(solutionJSON)
-
 	response := []interface{}{"cptR", solutionB64}
 	packedResponse, err := msgpack.Marshal(response)
 	if err != nil {
 		return fmt.Errorf("failed to encode captcha response: %w", err)
 	}
-
 	err = conn.WriteMessage(websocket.BinaryMessage, append(packedResponse, 0x00, 0x00))
 	if err != nil {
 		return fmt.Errorf("failed to send captcha response: %w", err)
 	}
-
 	fmt.Printf("Solved captcha: %d (took %dms)\n", solution, elapsed)
 	time.Sleep(1 * time.Second)
 
@@ -73,10 +64,8 @@ func SolveCaptcha(conn *websocket.Conn, captchaMessage []interface{}) error {
 
 func solveChallengeSync(algorithm, challenge, salt string, maxNumber int) int {
 	target := challenge
-
 	for i := 0; i <= maxNumber; i++ {
 		data := salt + strconv.Itoa(i)
-
 		var hash string
 		if algorithm == "SHA-256" {
 			h := sha256.Sum256([]byte(data))
@@ -84,11 +73,38 @@ func solveChallengeSync(algorithm, challenge, salt string, maxNumber int) int {
 		} else {
 			continue
 		}
-
 		if hash == target {
 			return i
 		}
 	}
 
 	return 0
+}
+
+func toString(data map[string]interface{}, key string) string {
+	if val, ok := data[key]; ok {
+		if str, ok := val.(string); ok {
+			return str
+		}
+	}
+	return ""
+}
+
+func toIntFromAny(value interface{}) int {
+	switch v := value.(type) {
+	case float64:
+		return int(v)
+	case int:
+		return v
+	case int32:
+		return int(v)
+	case int64:
+		return int(v)
+	case string:
+		if i, err := strconv.Atoi(v); err == nil {
+			return i
+		}
+	}
+	return 0
+
 }
