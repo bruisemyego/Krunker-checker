c2bbbbf0cb66fefc1cdaf45069cf849621085938
b9nk <wackoalt@gmail.com>
2025-08-25 19:11:32 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,53 @@
+// main.go
+
 package main
 
 import (
 	"fmt"
-	"krunker-checker/services"
+	"krunker-checker/src"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 const (
-	ColorCyan  = "\033[36m"
-	ColorReset = "\033[0m"
+	ColorCyan   = "\033[36m"
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorPurple = "\033[35m"
+	ColorBold   = "\033[1m"
 )
 
 func main() {
-	if err := services.ProcessAccounts(); err != nil {
-		fmt.Printf("Error processing accounts: %v\n", err)
-		return
-	}
+	fmt.Print("\033[H\033[2J")
+	printHeader()
 
-	fmt.Println(ColorCyan + "\nchecking completed!" + ColorReset)
-	fmt.Println(ColorCyan + "Check the results." + ColorReset)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan error, 1)
+	go func() {
+		done <- src.ProcessAccounts()
+	}()
+
+	select {
+	case err := <-done:
+		if err != nil {
+			fmt.Printf(ColorRed+"error processing accounts: %v\n"+ColorReset, err)
+			os.Exit(1)
+		}
+
+		fmt.Println(ColorGreen + "\nchecking completed!" + ColorReset)
+
+	case <-sigChan:
+		fmt.Println(ColorCyan + "\n\nShutting down..." + ColorReset)
+		os.Exit(0)
+	}
+}
+
+func printHeader() {
+	fmt.Printf(ColorBold + ColorCyan + "               Krunker Account Checker - @cleanest\n" + ColorReset)
+	fmt.Printf(ColorPurple + " 			   Version: 3.00\n\n" + ColorReset)
 }
